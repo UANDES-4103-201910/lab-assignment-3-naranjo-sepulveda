70a619f729f05daec088f6337a2e588265f4fd44
beannama <ben.nar.95@gmail.com>
2019-03-27 15:07:38 -0300

    first method

--- ticketsales/app/models/user.rb
+++ ticketsales/app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
 
   def most_expensive_ticket_bought()
-    ticketOrder = TicketOrder.where()
-
+    user_ticket = User.find(self.id).joins(:TicketOrder).joins(:TicketTypeInformation).order(:price).first()[:price]
+    return user_ticket
   end
 
   def most_expensive_ticket_bought_between(start_date,end_date)
--- ticketsales/db/migrate/20190327134838_create_ticket_type_informations.rb
+++ ticketsales/db/migrate/20190327134838_create_ticket_type_informations.rb
@@ -2,7 +2,7 @@ class CreateTycketTypeInformations < ActiveRecord::Migration[5.2]
   def change
     create_table :ticket_type_informations do |t|
       t.references :Event, foreign_key: true
-      t.integer :prince
+      t.integer :price
       t.string :category
 
       t.timestamps
