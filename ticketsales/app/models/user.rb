bdae020feac2600dce8ad64c66c01fbbc7e1e3c6
Your Name <you@example.com>
2019-03-27 13:21:27 -0300

    minor changes

--- ticketsales/app/models/user.rb
+++ ticketsales/app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+
+  def most_expensive_ticket_bought()
+    ticketOrder = TicketOrder.where()
+
+  end
+
+  def most_expensive_ticket_bought_between(start_date,end_date)
+
+  end
+
+  def last_event()
+
+  end
+
 end
